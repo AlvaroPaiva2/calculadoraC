7eeee472143021585ab449bdfbcb5e9ad396b561
Alvaro Paiva <153731229+AlvaroPaiva2@users.noreply.github.com>
2024-03-29 11:24:47 -0300

    first commit

---
+++ calcC++.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Calc em C++" << endl;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+
+    cout << "Digite o operador: ";
+    cin >> op;
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    switch (op) {
+        case '+':
+            cout << "Resultado: " << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Resultado: " << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Resultado: " << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Resultado: " << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            else
+                cout << "Erro: Divisao por zero." << endl;
+            break;
+        default:
+            cout << "Operador inválido." << endl;
+            break;
+    }
+
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
